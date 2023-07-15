0803143b1e45869f684f7c5be816eaaf19e8ab97
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-07-15 15:39:29 -0400

    declared source arrays

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//Source Arrays
+var lowerCase = ["a", "b", "c"];
+var upperCase = ["A", "B", "C"]
+var number = ["1", "2", "3"];
+var special = ["!", "@", "#"];
+
+console.log(lowerCase);
+console.log(upperCase);
+console.log(number);
+console.log(special);
