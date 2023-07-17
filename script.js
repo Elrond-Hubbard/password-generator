ec8d976697dd24e411569b8fb2be4880c4f1795b
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-07-17 18:03:21 -0400

    consolidated function calls into final generator function

--- script.js
+++ script.js
@@ -21,22 +21,19 @@ var hasLower = true;
 var hasUpper = true;
 var hasNumber = true;
 var hasSpecial = true;
-var passwordLength = 8;
+var passwordLength = 28;
 
 
-// Push selected characters to Concat Array based on User Options
+// This function will push selected characters to Concat Array based on User Options
 function hasOption(x, y) {
   if (x) {
     concatArray.push(...y);
   }
 }
-hasOption(hasLower, lowerCase);
-hasOption(hasUpper, upperCase);
-hasOption(hasNumber, number);
-hasOption(hasSpecial, special);
 
 
-// Populate New Password Array with random Concat Array Indices
+// This loop will populate New Password Array with 
+// random Concat Array indices.
 function generatePassword() {
   for (i = 0; i < passwordLength; i++) {
     newPassword.push(concatArray[Math.floor(Math.random() * concatArray.length)]);
@@ -44,31 +41,33 @@ function generatePassword() {
 }
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-///// INTERFACE /////
-
-
-// Validate Checkbox Input
-function getCheck() {
-  var checkBox = document.querySelector("#lowerbox");
+// This function will be invoked by the generator to
+// return the status of each checkbox.
+function getCheck(x) {
+  var checkBox = document.querySelector(x);
   if (checkBox.checked) {
-    console.log("lowercase true");
     return true;
   }
   else {
-    console.log("lowercase false");
     return false;
   }
 }
 
 // Write password to the #password input
 function writePassword() {
+  concatArray.length = 0;
+  hasLower = getCheck("#lowerbox");
+  hasUpper = getCheck("#upperbox");
+  hasNumber = getCheck("#numberbox");
+  hasSpecial = getCheck("#specialbox");
+  hasOption(hasLower, lowerCase);
+  hasOption(hasUpper, upperCase);
+  hasOption(hasNumber, number);
+  hasOption(hasSpecial, special);
   generatePassword();
   var password = newPassword.join("");
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  console.log(password);
   newPassword.length = 0;
 }
 
