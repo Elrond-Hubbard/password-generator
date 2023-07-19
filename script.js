43390aa1869caa049d967aa041c06d8ddb02c598
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-07-19 17:45:57 -0400

    removed canvas (debug)

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ generateBtn.addEventListener("click", writePassword);
 
 ///// MATRIX RAIN EFFECT /////
 
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight * 0.4;
+// const canvas = document.getElementById("canvas");
+// const context = canvas.getContext("2d");
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight * 0.4;
